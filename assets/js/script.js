a00aeaacc9bd6c3376823df2534c5b37e2095696
Gabby Hernandez <gabbyhdezz697@gmail.com>
2022-03-01 12:58:35 -0500

    add html details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log(test)
+
+const posters = document.querySelectorAll('.img')
+
+console.log(posters)
+
+fetch("https://imdb-api.com/en/API/MostPopularMovies/k_tgoqrhd7", {
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+  cache: 'reload',
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    let posterData = []
+    let allMovies = data.items
+    for (let i = 0; i < 15; i++) {
+        const movie = allMovies[i];
+        posterData.push(movie.image)
+        console.log(movie)
+        console.log(posterData)
+
+    } 
+    posterData.forEach((element, i) => posters[i].innerHTML = `<img src="${element}">`) 
+  })
+    
