14a02536f15e9823cfed7fe79670af3c72bc97d4
Lyere <BPpearsall@gmail.com>
2022-03-02 10:31:25 -0500

    fixed broken titles variable

--- assets/js/movieInfo.js
+++ assets/js/movieInfo.js
@@ -3,6 +3,17 @@ const titles = document.querySelectorAll('.img')
 
 console.log(posters)
 
-let APIKEY = "k_8ges1rmu"
+let APIKEY = "k_srm6hq6z"
 https://imdb-api.com/en/API/Title/
 let popularMoviesUrl = `https://imdb-api.com/en/API/MostPopularMovies/${APIKEY}`
+let titleSearchUrl = `https://imdb-api.com/en/API/Title/${APIKEY}/${movieId}`
+
+var getMovieId = function () {
+    // This is coming from the URL search bar in the browser. It is what comes after the `?`.
+    var queryString = document.location.search;
+    var movieId = queryString.split('=')[1];
+    console.log(movieId)
+
+}
+  
+getMovieId()
