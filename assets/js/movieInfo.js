bdab588b7d92bca7f63d5f2eea97fb083145ccc0
Lyere <BPpearsall@gmail.com>
2022-03-02 10:43:51 -0500

    successfully console logs individual specified movieInfo

--- assets/js/movieInfo.js
+++ assets/js/movieInfo.js
@@ -3,17 +3,9 @@ const titles = document.querySelectorAll('.img')
 
 console.log(posters)
 
+let queryString = document.location.search;
+let movieId = queryString.split('=')[1];
 let APIKEY = "k_srm6hq6z"
-https://imdb-api.com/en/API/Title/
 let popularMoviesUrl = `https://imdb-api.com/en/API/MostPopularMovies/${APIKEY}`
 let titleSearchUrl = `https://imdb-api.com/en/API/Title/${APIKEY}/${movieId}`
 
-var getMovieId = function () {
-    // This is coming from the URL search bar in the browser. It is what comes after the `?`.
-    var queryString = document.location.search;
-    var movieId = queryString.split('=')[1];
-    console.log(movieId)
-
-}
-  
-getMovieId()
