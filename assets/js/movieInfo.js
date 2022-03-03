75103a621e66fb0e98624c12343aa1c68ffea7d7
Marcus Walby <marcuswalby24@gmail.com>
2022-03-03 10:50:29 -0500

    home button change

--- assets/js/movieInfo.js
+++ assets/js/movieInfo.js
@@ -115,3 +115,5 @@ fetch(titleSearchUrl, {})
         videoEl.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${data.items[0].id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
       });
   });
+
+  
